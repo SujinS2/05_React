101206e16d0b5e628a81f0f17dfd2c9ee9f301bb
Sujin <chosujin517@gmail.com>
2024-08-23 15:35:58 +0900

    5교시

--- 02-2_todo/src/App.js
+++ 02-2_todo/src/App.js
@@ -20,7 +20,22 @@ function App() {
       isDone: false,
     },
   ]);
-  const addTodo = () => {};
+  const addTodo = () => {
+    const isLike = false;
+    const todoContent = text;
+    const isDone = false;
+    const today = new Date();
+    const year = today.getFullYear();
+    const month =
+      today.getMonth() + 1 < 10
+        ? "0" + (today.getMonth() + 1)
+        : today.getMonth() + 1; //getMonth()는 0부터 시작하므로
+    const date = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
+    const regDate = `${year}-${month}-${date}`;
+    const todo = { isLike, todoContent, regDate, isDone };
+    setTodoList([...todoList, todo]);
+    setText("");
+  };
 
   return (
     <div className="todo-wrap">
@@ -33,6 +48,12 @@ function App() {
             type="text"
             name="todo-text"
             onChange={changeValue}
+            onKeyUp={(e) => {
+              if (e.key == "Enter" || e.keyCode === 13) {
+                // 예전 브라우저도 지원하기 위해 둘다 넣어줌
+                addTodo();
+              }
+            }}
             value={text}
           />
           <button onClick={addTodo}>등록</button>
@@ -40,11 +61,28 @@ function App() {
         <div className="todo-list-wrap">
           {todoList.map((todo, index) => {
             return (
-              <ul key={"todo" + index}>
-                <li className="todo-like">{todo.isLike}</li>
-                <li className="todo-text">{todo.todoContent}</li>
+              <ul key={"todo" + index} className="todo">
+                <li className="todo-like" onClick={like}>
+                  {todo.isLike ? (
+                    <span className="material-icons">favorite</span>
+                  ) : (
+                    <span className="material-icons">favorite_border</span>
+                  )}
+                </li>
+                <li
+                  className={todo.isDone ? "todo-text todo-done" : "todo-text"}
+                >
+                  {todo.todoContent}
+                </li>
                 <li className="todo-date">{todo.regDate}</li>
-                <li className="todo-btn">{todo.isDone}</li>
+                <li className="todo-btn">
+                  {todo.isDone ? (
+                    ""
+                  ) : (
+                    <span className="material-icons done">verified</span>
+                  )}
+                  <span className="material-icons delete">backspace</span>
+                </li>
               </ul>
             );
           })}
